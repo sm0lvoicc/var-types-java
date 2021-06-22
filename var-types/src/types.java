507fd8fd396ed67b33dc256c8ea5fccf33524114
sm0lvoicc <smallvoiceofcalmandreason@gmail.com>
2021-06-22 16:05:04 +0530

    forgot

---
+++ var-types/src/types.java
@@ -0,0 +1,22 @@
+public class types {
+    public static void main(String[] args){
+        /*
+        so the basic types of variables are
+        > integer (int)
+        > float (float)
+        > decimal, or double? big numbers? (double)
+        > boolean (boolean)
+        > byte (byte)
+        > character (char)
+        > string (String)
+         */
+        int a = 3, b = 7;
+        float d = 3.14f;
+        double g = 3.145678990234242556, h = 84568456;
+        char k = 'j';
+        String l = "heh";
+        System.out.println(a + b); //output = 10
+        System.out.println(k); //output = j
+        System.out.println(l + k); //output = hehj
+    }
+}
